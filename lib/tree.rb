0f1114898b25460ab1933afb865c87c4d69e9801
Ajith Meleppat <87268944+ajithmeleppat@users.noreply.github.com>
2026-05-26 10:47:53 +0500

    Add balance check, rebalance and traversal methods

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'node'
+require 'pry-byebug'
+
 # Represents a binary search tree
 class Tree
   def initialize(array)
@@ -42,12 +44,103 @@ class Tree
   def delete(value)
     return nil unless include?(value)
 
-    parent, node = get_node(value)
-    children = number_of_children(node)
-    handle_children(parent, node, children)
+    parent, node, children = get_node(value)
+    if children == 2
+      delete_node_with_two_children(node)
+    elsif children == 1
+      delete_node_with_one_child(parent, node)
+    else
+      delete_leaf(parent, node)
+    end
+    self
+  end
+
+  def depth(value)
+    return nil unless include?(value)
+
+    node = @root
+    depth = 0
+    until node.value == value
+      depth += 1
+      node = node.value > value ? node.left : node.right
+    end
+    depth
+  end
+
+  def height(value = nil, node = @root)
+    return nil unless include?(value)
+
+    node = node.value > value ? node.left : node.right until node.value == value
+    calculate_height_at_node(node)
+  end
+
+  def balanced?(node = @root)
+    return true if node.nil?
+
+    left_height = calculate_height_at_node(node.left)
+    right_height = calculate_height_at_node(node.right)
+    return false if left_height - right_height > 1 || right_height - left_height > 1
+
+    return true if balanced?(node.left) && balanced?(node.right)
+
+    false
+  end
+
+  def level_order
+    return to_enum(:level_order) unless block_given?
+
+    queue = []
+    node = @root
+    queue << node
+    until queue.empty?
+      node = queue.shift
+      yield node.value
+      queue << node.left if node.left
+      queue << node.right if node.right
+    end
+  end
+
+  def inorder(node = @root, &block)
+    return to_enum(:inorder, node) unless block_given?
+    return nil if node.nil?
+
+    inorder(node.left, &block)
+    yield node.value
+    inorder(node.right, &block)
+  end
+
+  def preorder(node = @root, &block)
+    return to_enum(:preorder, node) unless block_given?
+    return nil if node.nil?
+
+    yield node.value
+    preorder(node.left, &block)
+    preorder(node.right, &block)
+  end
+
+  def postorder(node = @root, &block)
+    return to_enum(:postorder, node) unless block_given?
+    return nil if node.nil?
+
+    postorder(node.left, &block)
+    postorder(node.right, &block)
+    yield node.value
+  end
+
+  def rebalance
+    array = inorder.to_a
+    @root = build_tree(array)
   end
 
   private
+
+  def calculate_height_at_node(node)
+    return -1 if node.nil?
+
+    left = calculate_height_at_node(node.left)
+    right = calculate_height_at_node(node.right)
+    1 + [left, right].max
+  end
 
   def build_tree(array)
     return nil if array.empty?
@@ -66,7 +159,8 @@ class Tree
       parent = node
       node = node.value > value ? node.left : node.right
     end
-    [parent, node]
+    children = number_of_children(node)
+    [parent, node, children]
   end
 
   def number_of_children(node)
@@ -76,16 +170,6 @@ class Tree
       1
     else
       2
-    end
-  end
-
-  def handle_children(parent, node, children)
-    if children.zero?
-      delete_leaf(parent, node)
-    elsif children == 1
-      delete_node_with_one_child(parent, node)
-    else
-      print 'two children'
     end
   end
 
@@ -103,6 +187,21 @@ class Tree
       parent.left = child
     else
       parent.right = child
+    end
+  end
+
+  def delete_node_with_two_children(node)
+    successor = node.right
+    parent = node
+    while successor.left
+      parent = successor
+      successor = successor.left
+    end
+    node.value = successor.value
+    if parent == node
+      node.right = successor.right
+    else
+      parent.left = successor.right
     end
   end
 end
--- main.rb
+++ main.rb
@@ -1,11 +1,28 @@
 require_relative 'lib/tree'
-# array = Array.new(15) { rand(0..100) }
-array = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+array = Array.new(15) { rand(0..100) }
 tree = Tree.new(array)
+
+def traversal(tree)
+  print 'Inorder: '
+  puts(tree.inorder { |value| print "#{value} " })
+  print 'Preorder: '
+  puts(tree.preorder { |value| print "#{value} " })
+  print 'Postorder: '
+  puts(tree.postorder { |value| print "#{value} " })
+end
+puts 'Original tree'
 tree.pretty_print
-puts 'deleting a leaf node'
-tree.delete(10)
+puts tree.balanced? ? 'Tree balanced' : 'Tree is not balanced'
+traversal(tree)
+tree.insert(100)
+tree.insert(110)
+tree.insert(120)
+puts 'Tree after adding some elements'
 tree.pretty_print
-puts 'deleting a node with one child'
-tree.delete(70)
+puts tree.balanced? ? 'Tree balanced' : 'Tree is not balanced'
+tree.rebalance
+puts 'Tree after rebalance'
 tree.pretty_print
+puts tree.balanced? ? 'Tree balanced' : 'Tree is not balanced'
+traversal(tree)
